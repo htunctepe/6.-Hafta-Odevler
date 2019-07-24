5e824f94c15d174306745b7630f4ca27c01c52d1
Huseyin Tunctepe <HTunctepe@ShadowNinja-2.local>
2019-07-24 03:14:29 +0200

    Huseyin Tunctepe 6.Hafta 1.Odev: Sayi Tahmini Oyunu

--- PickANumber.py
+++ PickANumber.py
@@ -8,16 +8,16 @@ Enter '-' if the number I guessed is too big  : """
 control = ''
 max = 101
 min = 1
-guess = int((max - min)/2)
+guess = int((max - min)/2) # Here basically starting with 50, this way the number possibilities will be halved
 times = 0
 try:
     while control != 'y':
         times += 1
         control = input(message.format(guess))
         if control == '-':
-            max = guess
+            max = guess         # Here we make sure the next guesses will definitely be smaller than our guess
         elif control == '+':
-            min = guess
+            min = guess         # Here we make sure the next guesses will definitely be bigger than our guess
         else:
             print("\nThe number you guessed is {}. The number is guessed at {}. time.".format(guess, times))
         guess = int((max + min)/2)
